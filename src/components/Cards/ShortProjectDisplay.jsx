c512cb0d45b7acf109a160c277d6f8e877a53ce7
Himanshipatel04 <himanshipatel0409@gmail.com>
2024-09-09 21:33:42 +0530

    short project card

--- src/components/Cards/ShortProjectDisplay.jsx
+++ src/components/Cards/ShortProjectDisplay.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import img1 from "../../assets/artificial.jpg";
 import { Link } from "react-router-dom";
 
 const ShortProjectDisplay = ({item}) => {
@@ -11,30 +10,30 @@ const ShortProjectDisplay = ({item}) => {
       {/*key={pro.id} to={`/projects/${pro.id}`}*/}
       <div
         style={{ boxShadow: "0 0 5px 0.2px blue" }}
-        className="h-[400px] w-80 rounded-lg flex flex-col items-center justify-start gap-2 p-3"
+        className="h-[400px] relative w-80 rounded-lg flex flex-col items-center justify-start gap-2 p-3"
       >
         <img
           className="h-[35%] w-[100%] object-cover shadow-md rounded-lg "
-          src={img1}
+          src={item.image}
           alt=""
         />
         <div className="flex w-full flex-col gap-2 text-md items-start justify-start">
          <p className="w-full text-center text-2xl text-blue-700 font-semibold">
-           Advanced ChatGpt
+           {item.title}
           </p>
           <p>
-            <b>Institution</b>: Indian Institute of Technology, Mumbai
+            <b>Institution</b>: {item.collegename}
           </p>
           <p>
-            <b>Domain</b>: Aritificial Intelligence
+            <b>Domain</b>: {item.domain}
           </p>
           <p>
             {slicer(
-              "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, nisi! Veniam sit asperiores obcaecati magnam molestias, similique quae a quia excepturi dignissimos laborum id vitae, aperiam qu Quidem cumque nesciunt commodi, dicta nihil ullam numquam consequatur enim labore quibusdam neque incidunt accusamus veniam illum perferendis minus sequi aliquam, provident itaque! Possimus sed exercitationem perspiciatis autem libero aspernatur, non est reiciendis quisquam corporis ipsum! Incidunt saepe ea iure dignissimos obcaecati. Odit voluptates numquam eius repellendus commodi laudantium consectetur quas autem est beatae et aliquid voluptatibus perspiciatis ipsa enim, blanditiis at adipisci delectus ex. Dolore, laborum error!"
+              item.description
             )}
           </p>
-          <Link className="w-full pt-2" to="/projectdetail">
-            <button className="text-white outline hover:bg-white hover:text-black text-xl hover:outline-2 hover:outline-blue-600 bg-blue-700 transition-all duration-200  font-semibold w-full rounded-lg p-2">
+          <Link className="w-72 left-4 absolute bottom-3 pt-2" to="/projectdetail">
+            <button className=" text-white outline hover:bg-white hover:text-black text-xl hover:outline-2 hover:outline-blue-600 bg-blue-700 transition-all duration-200  font-semibold w-full rounded-lg p-2">
               Read Project
             </button>
           </Link>
